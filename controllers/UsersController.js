350285b696ff7e9a8cc9268983e63bb4f887f634
bek2518 <bereketb20@hotmail.com>
2023-06-27 20:31:47 +0300

    Added body handler in server and changed controller to async

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,28 +2,31 @@ import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 module.exports = class UsersController {
-  static postNew(req, res) {
+  static async postNew(req, res) {
     const { email } = req.body;
     const { password } = req.body;
 
     if (!email) {
       res.statusCode = 400;
       res.json({ error: 'Missing email' });
+      return
     }
 
     if (!password) {
       res.statusCode = 400;
       res.json({ error: 'Missing password' });
+      return
     }
 
-    const existingEmail = dbClient.db().collection('users').findOne({ email });
+    const existingEmail = await dbClient.db.collection('users').findOne({ email });
     if (existingEmail) {
       res.statusCode = 400;
       res.json({ error: 'Already exist' });
+      return
     }
 
     const hashesPassword = sha1(password);
-    const newUser = dbClient.db().collection('users').insertone({ email, password: hashesPassword });
+    const newUser = await dbClient.db.collection('users').insertone({ email, password: hashesPassword });
     res.statusCode = 201;
     res.json({ id: newUser.insertedId, email });
   }
